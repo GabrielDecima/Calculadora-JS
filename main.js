0dbed54f39cf44802a07cfd7c6642c9618d40854
Gabriel Decima <gabrieldecima1028@gmail.com>
2023-04-17 15:22:57 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const visor = document.getElementById("visor");
+const aux ="";
+const clear = document.getElementById("clear");
+
+
+
+
+clear.addEventListener('click',()=>{
+   
+    visor.innerText = clear.innerText;
+  
+})
